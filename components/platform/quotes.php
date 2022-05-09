207ba732da39bae0dd619f2fb0ba578ec85074ea
Rogelio Oseguera de la Cruz <rogeliooseguera16@gmail.com>
2022-05-09 16:57:20 -0500

    WIP Quotes section

--- components/platform/quotes.php
+++ components/platform/quotes.php
@@ -1,16 +1,39 @@
-<section class="quotes__section w-screen bg-gray-secondary px-12 lg:px-0">
-  <div class="quotes__slider flex justify-center items-center h-full">
-    <div class="quotes__card px-12 py-12 my-20">
-      <div class="dquote dquote-top"><span>“</span></div>
-      <div class="dquote dquote-bottom"><span>”</span></div>
-      <p class="text-lg">Certilytics' product set is built with modern tools and techniques, including open source software and tools.
-        ...Its modern technology stack distinguishes it from other payer-oriented analytics vendors. In addition to technology, Certilytics offers implementation services, ongoing product maintenance and resourcebundle_localessupport,
-        and data science-services.
-      </p>
-      <!-- <p class=" text-xl font-bold">—Chilmark Research 2019
-        Payer Analytics Market 
-        Trends Report
-      </p> -->
+<section class="slider__section p-0 lg:pt-20 lg:pb-20">
+  <div class="container relative">
+    <div class="absolute w-full h-full flex justify-between items-center">
+      <button type="button" class="slider-prev absolute h-24 w-28">⬅️</button>
+      <button type="button" class="slider-next absolute h-24 w-28">➡️</button>
+    </div>
+    <div class="quotes__slider">
+      <div class="slider__slide relative mx-auto shadow-none flex-col lg:flex-row lg:items-center lg:shadow-[8px_8px_0px_#011b3a51] p-8 lg:p-16 bg-white">
+        <div class="slider__slide__image flex justify-center px-8 lg:p-0">
+          <img src="<?php echo get_template_directory_uri()?>/dist/assets/images/platform/Platform_Photo_1-01.png"/>
+        </div>
+        <div class="slider__slide__text px-8 pt-8 lg:pr-0 lg:pt-0 lg:pl-10">
+          <p class="slider__slide__text--paragraph text-black	font-bold text-2xl lg:text-3xl">“The configurability of both the data layer and analytics layer makes integration seamless.”</p>
+          <p class="slider__slide__text--legend text-black italic text-2xl lg:text-3xl">–Self-funded employer customer</p>
+        </div>
+      </div>
+
+      <!-- <div class="slider__slide items-center	relative mx-auto shadow-none lg:shadow-[8px_8px_0px_#011b3a51] p-8 lg:p-16 bg-white">
+        <div class="slider__slide__image flex justify-center">
+          <img src="<?php echo get_template_directory_uri()?>/dist/assets/images/platform/Platform_Photo_2-01.png"/>
+        </div>
+        <div class="slider__slide__text pl-5 lg:pl10">
+          <p class="slider__slide__text--paragraph text-lg md:text-xl lg:text-3xl">"The configurability of both the data layer and analytics layer makes integration seamless.”</p>
+          <p class="slider__slide__text--legend italic text-lg md:text-xl lg:text-3xl">–Self-funded employer customer</p>
+        </div>
+      </div>
+
+      <div class="slider__slide items-center	relative mx-auto shadow-none lg:shadow-[8px_8px_0px_#011b3a51] p-8 lg:p-16 bg-white">
+        <div class="slider__slide__image flex justify-center">
+          <img src="<?php echo get_template_directory_uri()?>/dist/assets/images/platform/Platform_Photo_3-01.png"/>
+        </div>
+        <div class="slider__slide__text pl-5 lg:pl10">
+          <p class="slider__slide__text--paragraph text-lg md:text-xl lg:text-3xl">“We’ve had some challenges finding this level of customization from other partners. This is exciting!”</p>
+          <p class="slider__slide__text--legend italic text-lg md:text-xl lg:text-3xl">–Provider health system customer</p>
+        </div>
+      </div> -->
     </div>
   </div>
 </section>
--- src/assets/js/platform.js
+++ src/assets/js/platform.js
@@ -1,191 +1,204 @@
-import getNavHeight from './utils/utils';
+import getNavHeight from "./utils/utils";
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+  const sliderOptions = {
+    inifinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
-    const sliderOptions = {
-        inifinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    }
+  let platformsData = [];
+  let currentIdx = 0;
+  let cycleTimer = null;
 
-    let platformsData = [];
-    let currentIdx = 0;
-    let cycleTimer = null;
+  function setHeroMarginTop() {
+    const { navHeight } = getNavHeight();
+    $(".platform-hero").css("margin-top", `${navHeight}px`);
+  }
 
-    function setHeroMarginTop() {
-        const { navHeight } = getNavHeight();
-        $('.platform-hero').css('margin-top', `${navHeight}px`);
-    };
+  function handleOptionColapse() {
+    $(".submenu__button").on("click", function (e) {
+      e.preventDefault();
+      const [element, arrow] = $(this).children();
+      $(arrow).toggleClass("rotate__arrow");
+      $(this).next().toggleClass("platforms__option--open");
+      $(this).next().children(":first").toggleClass("platforms__option--open");
+    });
+  }
 
-    function handleOptionColapse() {
-        $('.submenu__button').on('click', function(e) {
-            e.preventDefault();
-            const [element, arrow] = $(this).children();
-            $(arrow).toggleClass('rotate__arrow');
-            $(this).next().toggleClass('platforms__option--open');
-            $(this).next().children(':first').toggleClass('platforms__option--open');
-        });
-    };
+  function createQuotesSlider() {
+    $(".quotes__slider").slick(sliderOptions);
+  }
 
-    function createQuotesSlider() {
-        $('.quotes__slider').slick(sliderOptions);
-    }
+  function createValidationSlider() {
+    $(".validation__slider").slick({ ...sliderOptions, dots: true });
+  }
 
-    function createValidationSlider() {
-        $('.validation__slider').slick({...sliderOptions, dots: true });
-    }
+  function revealDataSlideContent() {
+    $(".data__slides__content__button").click(function () {
+      const parent = $(this).parent();
+      parent.toggleClass(".slide__open");
+      const isOpen = parent.hasClass(".slide__open");
+      const overlay = parent.find(".data__slide__overlay");
+      const description = parent.find(".data__slides__content__description");
+      // const textHeight = !isOpen ? '40vh' : `${parent.find('.data__slide__text').height()}px`;
+      const textHeight = `${parent.find(".data__slide__text").height()}px`;
+      const buttonAnimation = !isOpen ? "rotate(0deg)" : "rotate(180deg)";
 
-    function revealDataSlideContent() {
-        $('.data__slides__content__button').click(function() {
-            const parent = $(this).parent();
-            parent.toggleClass('.slide__open');
-            const isOpen = parent.hasClass('.slide__open');
-            const overlay = parent.find('.data__slide__overlay');
-            const description = parent.find('.data__slides__content__description');
-            // const textHeight = !isOpen ? '40vh' : `${parent.find('.data__slide__text').height()}px`;
-            const textHeight = `${parent.find('.data__slide__text').height()}px`;
-            const buttonAnimation = !isOpen ? 'rotate(0deg)' : 'rotate(180deg)';
+      overlay.toggle();
+      description.css("height", textHeight);
+      parent.find("button").css("transform", buttonAnimation);
+    });
+  }
 
-            overlay.toggle();
-            description.css('height', textHeight);
-            parent.find('button').css('transform', buttonAnimation);
+  function hideArrows() {
+    const leftArrow = $(".platforms__button.left");
+    const rightArrow = $(".platforms__button.right");
+    leftArrow.show();
+    rightArrow.show();
+    if (!currentIdx) leftArrow.hide();
+    if (currentIdx + 1 === platformsData.length) rightArrow.hide();
+  }
 
-        });
-    }
+  function setPlatformsSlideData(idx) {
+    const [leftArrow, title, icon, platformDescription, rightArrow] =
+      $("#platforms-slide").children();
+    const { icon_desktop, text, description } = platformsData[idx];
+    title.innerHTML = text;
+    icon.firstChild.src = icon_desktop;
+    platformDescription.innerHTML = description;
+    $(".active__platform").removeClass("active__platform");
+    $(`[name="${text}"]`).addClass("active__platform");
+    currentIdx = idx;
+    hideArrows();
+  }
 
-    function hideArrows() {
-        const leftArrow = $('.platforms__button.left');
-        const rightArrow = $('.platforms__button.right');
-        leftArrow.show();
-        rightArrow.show();
-        if (!currentIdx) leftArrow.hide();
-        if (currentIdx + 1 === platformsData.length) rightArrow.hide();
-    }
+  function populatePlatformSlide() {
+    const data = JSON.parse(
+      document.getElementById("platforms-data").innerHTML
+    );
+    const acceleratorData = Object.values(data.platform_1.buttons);
+    const nucleusData = Object.values(data.platform_2.buttons);
+    const newPlatformsData = [...acceleratorData, ...nucleusData].map(
+      (data, idx) => ({ ...data, idx })
+    );
+    platformsData = newPlatformsData;
+    setPlatformsSlideData(0);
+  }
 
-    function setPlatformsSlideData(idx) {
-        const [leftArrow, title, icon, platformDescription, rightArrow] = $('#platforms-slide').children();
-        const { icon_desktop, text, description } = platformsData[idx];
-        title.innerHTML = text;
-        icon.firstChild.src = icon_desktop;
-        platformDescription.innerHTML = description;
-        $('.active__platform').removeClass('active__platform');
-        $(`[name="${text}"]`).addClass('active__platform');
-        currentIdx = idx;
-        hideArrows();
-    }
+  function handleSlideCycle() {
+    $(".platforms__button").click(({ currentTarget: { name } }) => {
+      const step = name === "platformsNext" ? 1 : -1;
+      const idxToUse = currentIdx + step;
+      setPlatformsSlideData(idxToUse);
+      clearInterval(cycleTimer);
+      if (name === "platformsPrev" && !currentIdx) $(this).hide();
+      if (name === "platformsNext" && currentIdx + 1 === platformsData.length)
+        $(this).hide();
+    });
+  }
 
-    function populatePlatformSlide() {
-        const data = JSON.parse(document.getElementById('platforms-data').innerHTML);
-        const acceleratorData = Object.values(data.platform_1.buttons);
-        const nucleusData = Object.values(data.platform_2.buttons);
-        const newPlatformsData = [...acceleratorData, ...nucleusData].map((data, idx) => ({...data, idx }));
-        platformsData = newPlatformsData;
-        setPlatformsSlideData(0);
-    }
+  function handlePlatformClick() {
+    $(".platforms__icon__button").click(({ currentTarget }) => {
+      const name = $(currentTarget.lastChild.firstChild).attr("name");
+      const nextPlatform = platformsData.find(
+        (platform) => platform.text === name
+      );
+      setPlatformsSlideData(nextPlatform.idx);
+      clearInterval(cycleTimer);
+    });
+  }
 
+  function addArrowToSlider() {
+    $(".slider-prev").click(function () {
+      $(".validation__slider").slick("slickPrev");
+    });
+    $(".slider-next").click(function () {
+      $(".validation__slider").slick("slickNext");
+    });
+  }
 
-    function handleSlideCycle() {
-        $('.platforms__button').click(({ currentTarget: { name } }) => {
-            const step = name === 'platformsNext' ? 1 : -1;
-            const idxToUse = currentIdx + step;
-            setPlatformsSlideData(idxToUse);
-            clearInterval(cycleTimer);
-            if (name === 'platformsPrev' && !currentIdx) $(this).hide();
-            if (name === 'platformsNext' && currentIdx + 1 === platformsData.length) $(this).hide();
-        });
-    }
+  function setCycle() {
+    if (currentIdx + 1 === platformsData.length)
+      return setPlatformsSlideData(0);
+    return setPlatformsSlideData(currentIdx + 1);
+  }
 
-    function handlePlatformClick() {
-        $('.platforms__icon__button').click(({ currentTarget }) => {
-            const name = $(currentTarget.lastChild.firstChild).attr('name');
-            const nextPlatform = platformsData.find(platform => platform.text === name);
-            setPlatformsSlideData(nextPlatform.idx);
-            clearInterval(cycleTimer);
-        })
-    }
-
-    function addArrowToSlider() {
-        $('.slider-prev').click(function() {
-            $('.validation__slider').slick('slickPrev');
-        });
-        $('.slider-next').click(function() {
-            $('.validation__slider').slick('slickNext');
-        });
-    }
-
-    function setCycle() {
-        if (currentIdx + 1 === platformsData.length) return setPlatformsSlideData(0);
-        return setPlatformsSlideData(currentIdx + 1);
-    }
-
-    function handleHoverStop(selector) {
-        $(selector).hover(function() {
-            clearInterval(cycleTimer);
-        }, function() {
-            cycleTimer = setInterval(setCycle, 20000);
-        });
-    }
-
-    function handleAutoCycle() {
+  function handleHoverStop(selector) {
+    $(selector).hover(
+      function () {
+        clearInterval(cycleTimer);
+      },
+      function () {
         cycleTimer = setInterval(setCycle, 20000);
-    };
+      }
+    );
+  }
 
-    //proven solution number animation
+  function handleAutoCycle() {
+    cycleTimer = setInterval(setCycle, 20000);
+  }
 
-    function hasDecimals(number) {
-        return (number - Math.floor(number));
+  //proven solution number animation
+
+  function hasDecimals(number) {
+    return number - Math.floor(number);
+  }
+
+  function calculateValue(number, idx) {
+    return number + idx;
+  }
+
+  function calculateDecimals(idx) {
+    if (idx === 0) return 1 / 10;
+    return idx / 10;
+  }
+
+  function getAnimationMultiplier(endNumber) {
+    let multiplier = 1;
+    if (endNumber >= 200) multiplier = endNumber / 50;
+    return { multiplier, speed: 20 };
+  }
+
+  function recursiveIncrease(idx, endNumber, element) {
+    const newEndNumber = hasDecimals(endNumber) ? endNumber * 10 : endNumber;
+    if (idx <= newEndNumber) {
+      const { multiplier, speed } = getAnimationMultiplier(endNumber);
+      element.textContent = hasDecimals(endNumber)
+        ? calculateDecimals(idx)
+        : idx;
+      setTimeout(() => {
+        recursiveIncrease(calculateValue(multiplier, idx), endNumber, element);
+      }, speed);
     }
+  }
 
-    function calculateValue(number, idx) {
-        return number + idx;
-    }
+  function animateNumberIncrease(el) {
+    const endNumber = +$(el).attr("data-number");
+    recursiveIncrease(0, endNumber, el);
+  }
 
-    function calculateDecimals(idx) {
-        if (idx === 0) return 1 / 10;
-        return idx / 10;
-    }
+  function handleProvenNumbersIncreaseAnimation() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) =>
+        entry.isIntersecting ? animateNumberIncrease(entry.target) : null
+      );
+    });
+    const numberElements = document.querySelectorAll(".increase__number");
+    numberElements.forEach((element) => observer.observe(element));
+  }
 
-    function getAnimationMultiplier(endNumber) {
-        let multiplier = 1;
-        if (endNumber >= 200) multiplier = endNumber / 50;
-        return { multiplier, speed: 20 };
-    }
-
-    function recursiveIncrease(idx, endNumber, element) {
-        const newEndNumber = hasDecimals(endNumber) ? endNumber * 10 : endNumber;
-        if (idx <= newEndNumber) {
-            const { multiplier, speed } = getAnimationMultiplier(endNumber);
-            element.textContent = hasDecimals(endNumber) ? calculateDecimals(idx) : idx;
-            setTimeout(() => {
-                recursiveIncrease(calculateValue(multiplier, idx), endNumber, element);
-            }, speed);
-        }
-    }
-
-    function animateNumberIncrease(el) {
-        const endNumber = +$(el).attr('data-number');
-        recursiveIncrease(0, endNumber, el);
-    }
-
-
-    function handleProvenNumbersIncreaseAnimation() {
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => (entry.isIntersecting) ? animateNumberIncrease(entry.target) : null)
-        });
-        const numberElements = document.querySelectorAll('.increase__number');
-        numberElements.forEach(element => observer.observe(element));
-    };
-
-    if (window.location.href.includes('platform')) {
-        setHeroMarginTop();
-        handleOptionColapse();
-        revealDataSlideContent();
-        createValidationSlider();
-        populatePlatformSlide();
-        handleSlideCycle();
-        handlePlatformClick();
-        addArrowToSlider();
-        handleAutoCycle();
-        handleProvenNumbersIncreaseAnimation();
-    }
+  if (window.location.href.includes("platform")) {
+    setHeroMarginTop();
+    handleOptionColapse();
+    revealDataSlideContent();
+    createValidationSlider();
+    populatePlatformSlide();
+    handleSlideCycle();
+    handlePlatformClick();
+    addArrowToSlider();
+    handleAutoCycle();
+    handleProvenNumbersIncreaseAnimation();
+    createQuotesSlider();
+  }
 });
