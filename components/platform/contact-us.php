abb5c46605994653c2df35fb358f01512792f843
Rogelio Oseguera de la Cruz <rogeliooseguera16@gmail.com>
2022-05-12 22:03:35 -0500

    WIP schedule demo

--- components/platform/contact-us.php
+++ components/platform/contact-us.php
@@ -1,25 +1,14 @@
-<section class="platform__contact__us min-h-screen flex flex-col lg:flex-row lg:items-center">
-  <div class="contat__us__image__container w-full lg:w-1/2 flex justify-center relative py-28">
-    <img src="<?php the_field('ofast_image') ?>" class=" z-10 opacity-100 w-10/12 lg:w-7/12" alt="" data-aos="fade-up">
-    <div class=" pulse__order-2 absolute">
-      <div class="dot-pulse"></div>
-    </div>
-    <div class=" pulse__order-3 absolute">
-      <div class="dot-pulse"></div>
-    </div>
-    <div class=" pulse__order-4 absolute">
-      <div class="dot-pulse"></div>
-    </div>
-    <div class=" pulse__order-5 absolute">
-      <div class="dot-pulse"></div>
+<section class="platform__contact__us min-h-screen">
+  <div class="contat__us__image__container w-full h-full lg:relative inline-block text-center">
+    <img src="<?php the_field('ofast_image') ?>" class="w-full block" alt="">
+    <div class="action__quote__container lg:absolute lg:top-1/3 lg:left-1/2">
+      <div class="w-full lg:w-3/4">
+        <p class="action__quote__container__text text-xl text-white lg:text-7xl lg:font-bold text-center" data-aos="fade-up">Are you ready to see the <span class="text-primary">future</span> through data?</p>
+        <div class="action__quote__container w-full mx-auto mt-10 py-10 flex lg:px-28 justify-center">
+          <a href="#" class=" w-8/12 bg-primary text-white font-bold rounded-lg text-xl text-center py-3" data-aos="fade-up">Watch Demo</a>
+        </div>
+      </div>
     </div>
   </div>
-  <div class="action__quote__container w-7/12 lg:w-1/2 mx-auto h-full flex justify-center items-center flex-col">
-    <div class="w-full lg:w-3/4">
-    <p class="text-xl lg:text-3xl lg:font-bold text-center" data-aos="fade-up">Are you ready to see the future through data?</p>
-    <div class="action__quote__container w-full mx-auto mt-10 py-10 flex lg:px-20 justify-center">
-      <a href="#" class=" w-8/12 bg-primary text-white font-bold rounded-lg text-xl text-center py-3" data-aos="fade-up">Watch Demo</a>
-    </div>
-    </div>
-  </div>
+  
 </section>
