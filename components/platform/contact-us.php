81a991a9d39fe0ad6bb402e1cd4b695fa141a5d9
Oswaldo Rangel <lebial@gmail.com>
2022-06-07 09:41:58 -0500

    Fix/last last fixes (#30)
    * added copy to proven solution
    
    * fixed several stuff on platform page
    
    * updated contact us section
    
    * fixed footer and deploy options sectoin

--- components/platform/contact-us.php
+++ components/platform/contact-us.php
@@ -2,7 +2,7 @@
   <div class="contact__us__image__container w-full lg:min-h-[70vh] relative text-center flex justify-end items-center">
     <img src="<?php the_field('ofast_image') ?>" class="w-full absolute top-0 left-0 z-[-1] h-full object-cover" alt="">
     <div class="action__quote__container w-full lg:w-1/2" data-aos="fade-up">
-      <div class="w-full lg:w-3/4 px-8 md:px-32 lg:px-0 pt-16 lg:pt-0">
+      <div class="w-full flex flex-col items-center lg:w-3/4 px-8 md:px-32 lg:px-0 pt-16 lg:pt-0">
         <p class="action__quote__container__text text-white font-bold text-center" >Are you ready to see the <span class="text-primary">future</span> through data?</p>
         <div class="action__quote__container__button w-full mx-auto mt-10 pb-14 flex lg:px-1 xl:px-2 justify-center">
           <a href="#" class=" w-7/12 lg:w-8/12 bg-primary text-white font-bold rounded-lg text-xl text-center py-3">Schedule Demo</a>
