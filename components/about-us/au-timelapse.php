f1b1f452b72f30c865ca7881475d94cc12779b94
Oswaldo Rangel <lebial@gmail.com>
2022-08-31 09:56:25 -0500

    WIP timeline

--- components/about-us/au-timelapse.php
+++ components/about-us/au-timelapse.php
@@ -1,48 +1,94 @@
-<section class="au__timeline w-full min-h-screen mt-12">
-    <div class="au__timeline__wrapper relative">
+<section class="au__timeline w-full mt-12 flex justify-center">
+    <div class="au__timeline__wrapper relative w-[90%] md:w-[80%] lg:w-[60%]">
         <div class="au__timeline__nav relative px-[12%]">
             <div class="date__container">
                 <div class="main-year ">
-                    <p >2014</p>
+                    <div class="circle__container">
+                        <p >2014</p>
+                    </div>
                 </div>
             </div>
             <div class="date__container">
-                <div class="main-year">
-                    <p>2015</p>
+              <div class="main-year">
+                <div class="circle__container">
+                  <p>2015</p>
                 </div>
+              </div>
             </div>
             <div class="date__container">
-                <div class="main-year">
-                    <p>2016</p>
+              <div class="main-year">
+                <div class="circle__container">
+                  <p>2016</p>
                 </div>
+              </div>
             </div>
             <div class="date__container">
-                <div class="main-year">
+              <div class="main-year">
+                <div class="circle__container">
                     <p>2017</p>
                 </div>
+              </div>
             </div>
             <div class="date__container">
-                <div class="main-year">
-                    <p>2018</p>
+              <div class="main-year">
+                <div class="circle__container">
+                  <p>2018</p>
                 </div>
+              </div>
             </div>
             <div class="date__container">
-                <div class="main-year">
-                    <p>2019</p>
+              <div class="main-year">
+                <div class="circle__container">
+                  <p>2019</p>
                 </div>
+              </div>
             </div>
         </div>
-        <div class="au__timeline__slider">
-            <div>2014</div>
-            <div>2015</div>
-            <div>2016</div>
-            <div>2017</div>
-            <div>2018</div>
-            <div>2019</div>
+        <div class="au__timeline__slider px-12">
+          <div>
+            <div class="w-full flex flex-col sm:flex-row items-center justify-center">
+              <div class="w-full md:w-1/2">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quidem aut molestiae voluptate dolorem nulla officia mollitia dolore</p>
+              </div>
+              <div class="w-full md:w-1/2">
+                <p class="text-center"><span class="text-primary">500K</span> lives on platform </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="w-full flex flex-col md:flex-row items-center justify-center">
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quidem aut molestiae voluptate dolorem nulla officia mollitia dolore</p>
+              <p class="text-center"><span class="text-primary">600K</span> lives on test </p>
+            </div>
+          </div>
+          <div>
+            <div class="w-full flex flex-col md:flex-row items-center justify-center">
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quidem aut molestiae voluptate dolorem nulla officia mollitia dolore</p>
+              <p class="text-center"><span class="text-primary">700K</span> lives on test2 </p>
+            </div>
+          </div>
+          <div>
+            <div class="w-full flex flex-col md:flex-row items-center justify-center">
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quidem aut molestiae voluptate dolorem nulla officia mollitia dolore</p>
+              <p class="text-center"><span class="text-primary">800K</span> lives on test3 </p>
+            </div>
+          </div>
+          <div>
+            <div class="w-full flex flex-col md:flex-row items-center justify-center">
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quidem aut molestiae voluptate dolorem nulla officia mollitia dolore</p>
+              <p class="text-center"><span class="text-primary">900K</span> lives on test4 </p>
+            </div>
+          </div>
+          <div>
+            <div class="w-full flex flex-col md:flex-row items-center justify-center">
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quidem aut molestiae voluptate dolorem nulla officia mollitia dolore</p>
+              <p class="text-center"><span class="text-primary">1000K</span> lives on test5 </p>
+            </div>
+          </div>
             
         </div>
 
-        <div class="au__timeline__buttons__container absolute w-full flex justify-between top-0">
+        <div class="au__timeline__buttons__container absolute w-full flex justify-between top-[10%]">
             <?php custom_slider_arrows('au__timeline__nav')?>
         </div>
     </div>
