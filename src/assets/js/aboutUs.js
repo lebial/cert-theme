904652e09130e532e64dce1e2216b50ea757b2dd
Oswaldo Rangel <lebial@gmail.com>
2022-09-01 10:45:57 -0500

    Feature/who we are timeline (#55)
    * WIP careers-section
    
    * WIP who we are timeline
    
    * Update css code in slider section
    
    * WIP timeline
    
    * WIP timeline section
    
    * WIP timeline
    
    * fixed multiple things on careers and timeline
    
    Co-authored-by: Rogelio Oseguera de la Cruz <rogeliooseguera16@gmail.com>

--- src/assets/js/aboutUs.js
+++ src/assets/js/aboutUs.js
@@ -1,34 +1,51 @@
-import getNavHeight, { addArrowToSlider } from './utils/utils';
+import getNavHeight, { addArrowToSlider } from "./utils/utils";
 
-jQuery(document).ready(function($) {
-
+jQuery(document).ready(function ($) {
   function setImageMargin() {
     const { navHeight } = getNavHeight();
-    $('.au__heading__image').css('margin-top', `${navHeight}px`);
+    $(".au__heading__image").css("margin-top", `${navHeight}px`);
   }
 
   function createTimelineSlider() {
     $('.au__timeline__nav').slick({
+      variableWidth: true,
       centerMode: true,
       dots: false,
       infinite: false,
-      slidesToShow: 5,
+      slidesToShow: 3,
       focusOnSelect: true,
-      asNavFor: '.au__timeline__slider'
+      asNavFor: '.au__timeline__slider',
+      responsive: [
+        {
+          breakpoint: 600,
+          slidesToShow: 3
+        }
+      ],
     });
-    $('.au__timeline__slider').slick({
+    $(".au__timeline__slider").slick({
       slidesToShow: 1,
       slidesToScroll: 1,
+      infinite: false,
       arrows: false,
       fade: true,
       draggable: false,
-      asNavFor: '.au__timeline__nav',
+      asNavFor: ".au__timeline__nav",
     });
   }
 
-  if (window.location.href.includes('who-we-are')) {
+  function createCareersSlider() {
+    $(".au__careers__slider").slick({
+      dots: false,
+      infinite: true,
+      slidesToShow: 1,
+    });
+  }
+
+  if (window.location.href.includes("who-we-are")) {
     setImageMargin();
     createTimelineSlider();
-    addArrowToSlider('.au__timeline__nav');
+    addArrowToSlider(".au__timeline__nav");
+    createCareersSlider();
+    addArrowToSlider(".au__careers__slider");
   }
 });
