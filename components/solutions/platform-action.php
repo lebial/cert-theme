8a4400186b518b73f892adeb562cc6804b2b1818
Rogelio Oseguera de la Cruz <rogeliooseguera16@gmail.com>
2022-09-06 09:58:15 -0500

    Add solution cards

--- components/solutions/platform-action.php
+++ components/solutions/platform-action.php
@@ -4,9 +4,9 @@
     <!-- <img src="<?php //the_field('demo_image') ?>" class="w-full absolute top-0 left-0 z-[-1] h-full object-cover" alt=""> -->
     <div class="w-2/4">
       <div class="w-full flex flex-col items-center px-8 md:px-32 lg:px-0 pt-16 lg:pt-0">
-        <h3 class="tracking-widest text-black font-normal text-center lg:text-5xl" ><?php the_field('demo_description') ?></h3>
+        <h3 class="tracking-widest text-black font-normal text-center lg:text-3xl 2xl:text-5xl" ><?php the_field('demo_description') ?></h3>
         <div class="w-full mx-auto mt-8 flex lg:px-1 xl:px-2 justify-center">
-          <a target="_blank" rel="noopener noreferrer" href="<?php the_field('demo_link_button')?>" class=" w-7/12 lg:w-4/12 bg-primary text-white font-bold rounded-lg text-xl text-center py-3"><?php the_field('demo_text_button') ?></a>
+          <a target="_blank" rel="noopener noreferrer" href="<?php the_field('demo_link_button')?>" class=" w-7/12 lg:w-5/12 2xl:w-4/12 bg-primary text-white font-bold rounded-lg lg:text-base 2xl:text-xl text-center py-3"><?php the_field('demo_text_button') ?></a>
         </div>
       </div>
     </div>
--- components/solutions/sets-certilytics-apart.php
+++ components/solutions/sets-certilytics-apart.php
@@ -1,12 +1,30 @@
-<section class="w-full h-full bg-gray-primary">
-    <h3>What Sets Certilytics Apart</h3>
-    <div class="cards w-full my-0 mx-auto grid grid-cols-3">
-        <div class="card">
-            <img src="" alt="" class="card__image">
-            <div class="card__content">
-                <p>Intuitive Insights</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur corrupti totam iste, harum magnam doloremque. Tempore labore cumque ratione, excepturi rem quaerat exercitationem. Qui alias veritatis laboriosam tempore! Hic, repellat!</p>
-            </div>
-        </div>
+<?php
+    function render_cards() {
+        $cards = get_field('cards');
+        foreach($cards as $card) {
+               echo '<div class="card lg:px-20 lg:py-8 2xl:px-40">';
+                    echo '<div class="card__image w-full flex justify-center">';
+                        echo '<p>Image</p>';
+                        //echo '<!-- <img src="'.$card['image'].'" alt="" class="card__image"> -->';
+                    echo '</div>';
+                    echo '<div class="card__content">';
+                        echo '<div class="card__content--title text-center lg:px-12">';
+                            echo '<h4 class="text-base font-bold">'.$card['title'].'</h4>';
+                        echo '</div>';
+                        echo '<div class="">';
+                            echo '<p class="lg:text-sm">'.$card['description'].'</p>';
+                        echo '</div>';
+                    echo '</div>';
+                echo '</div>';
+        }
+    }
+?>
+
+<section class="w-full h-full bg-gray-primary pb-20">
+    <div class="text-center">
+        <h3 class="w-full lg:mb-12 2xl:mb-24"><?php the_field('title')?></h3>
+    </div>
+    <div class="cards w-full my-0 mx-auto grid lg:grid-cols-3 lg:gap-1 2xl:gap-20">
+        <?php render_cards()?>
     </div>
 </section>
