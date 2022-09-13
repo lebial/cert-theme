7a366a29f2ee51f24a72a66efb9d0c6305cd0cd6
Rogelio Oseguera de la Cruz <rogeliooseguera16@gmail.com>
2022-09-13 18:36:01 -0500

    Add images to solutions page

--- components/solutions/sets-certilytics-apart.php
+++ components/solutions/sets-certilytics-apart.php
@@ -2,10 +2,9 @@
     function render_cards() {
         $cards = get_field('cards');
         foreach($cards as $card) {
-               echo '<div class="card px-12 py-4 flex md:flex-col lg:px-8 lg:py-8 2xl:px-24">';
-                    echo '<div class="card__image w-5/12 md:w-full flex justify-center">';
-                        echo '<p>Image</p>';
-                        //echo '<!-- <img src="'.$card['image'].'" alt="" class="card__image"> -->';
+               echo '<div class="card px-12 py-4 space-x-8 md:space-x-0 flex md:flex-col lg:px-8 lg:py-8 2xl:px-24">';
+                    echo '<div class="card__image w-5/12 md:w-full h-[45%] md:h-[27%] flex justify-end md:justify-center">';
+                        echo '<img class="card__image" src="'.$card['image'].'" alt="">';
                     echo '</div>';
                     echo '<div class="card__content w-7/12 md:w-full">';
                         echo '<div class="card__content--title text-left md:text-center pt-0 pb-4 md:pt-4 md:pb-4 lg:px-8">';
@@ -20,7 +19,7 @@
     }
 ?>
 
-<section class="w-full h-full bg-gray-primary pb-20">
+<section class="w-full h-full bg-white pb-20">
     <div class="w-full md:w-8/12 lg:w-9/12 flex flex-col items-center mx-auto">
         <p class="text-2xl md:text-4xl py-8 md:py-12 text-center"><?php the_field('title')?></p>
         <div class="cards w-full grid lg:grid-cols-3 lg:gap-1 2xl:gap-20">
