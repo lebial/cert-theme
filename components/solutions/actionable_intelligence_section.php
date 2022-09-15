d30da5b500f9dea3a10e84a6b118ac167beb5d85
Oswaldo Rangel <lebial@gmail.com>
2022-09-15 18:10:25 -0500

    wip adding assets, intuitive insights missing (#68)

--- components/solutions/actionable_intelligence_section.php
+++ components/solutions/actionable_intelligence_section.php
@@ -1,34 +1,42 @@
+<?php 
+  function render_data_list() {
+    $data_list = get_field('data_list');
+    foreach ($data_list as $item) {
+        echo '<li>'.$item.'</li>';
+    };
+  }
+
+  function render_results_list() {
+    $result_list = get_field('results_list');
+    foreach ($result_list as $result) {
+        echo '<li>'.$result.'</li>';
+    }
+  }
+?>
+
 <section class="actionable_intelligence bg-dark-background w-full py-20 flex justify-center">
     <div class="data__container w-10/12 lg:w-8/12">
-        <h2 class="text-white text-2xl lg:text-4xl text-center">Actionable intelligence at your fingertips with AI-powered data warehousing</h2>
+        <h2 class="text-white text-2xl lg:text-4xl text-center"><?php the_field('actionable_intelligence_section_title')?></h2>
         <div class="flex flex-col md:flex-row w-full mt-20 md:mt-28 ">
             <div class=" w-full md:w-3/12 text-white flex flex-col items-center">
-                <div><p class="text-white font-bold ttext-center">Data</p></div>
+                <div><p class="text-white font-bold text-center"><?php the_field('data_title')?></p></div>
                 <ul class="data__list list-disc text-sm lg:text-base">
-                    <li>Medical Claims</li>
-                    <li>Rx Claims</li>
-                    <li>401(K)</li>
-                    <li>Disability</li>
-                    <li>Workers'Comp</li>
+                    <?php render_data_list()?>
                 </ul>
             </div>
             <div class="w-full md:w-6/12 flex justify-center mt-12 md:mt-0">
-                <div class="bg-white h-64 aspect-square rounded-full"></div>
+                <div class="bg-white h-64 aspect-square rounded-full"><img src="<?php the_field('actionable_intelligence_graphic')?>" alt=""></div>
             </div>
             <div class="w-full md:w-3/12 text-white flex flex-col items-center mt-12 md:mt-0">
-                <div><p class="text-white font-bold ttext-center">Results</p></div>
+                <div><p class="text-white font-bold ttext-center"><?php the_field('results_title')?></p></div>
                 <ul class="data__list text-sm lg:text-base">
-                    <li>Improved employee helath and well-being</li>
-                    <li>Fully connected data with answers at your fingertips</li>
-                    <li>Cost savings driven by better health outcomes</li>
+                    <?php render_results_list()?>
                 </ul>
             </div>
         </div>
         <div class="flex justify-center mt-20 md:mt-28">
-            <p class="text-white text-sm lg:text-base">Simplify and accelerate your decision-making with a next-generation data warehouse
-                that transforms your benefits data into actionable intelligence. Improve benefit
-                design, coordinate care across vendors, and incentivize health partners to shift from 
-                free-for-service to value.
+            <p class="text-white text-sm lg:text-base">
+                <?php the_field('results_description')?>
             </p>
         </div>
     </div>
--- components/solutions/case_study_section.php
+++ components/solutions/case_study_section.php
@@ -2,18 +2,16 @@
     <div class="content__container w-10/12 md:w-7/12 flex flex-col lg:flex-row">
         <div class="case__study__description w-full lg:w-7/12">
             <p class="text-2xl lg:text-4xl">
-                <b>Case study:</b> se how employers pinpoint savings opportunities and
-                measure program value
+                <?php the_field('case_study_title')?>
             </p>
             <p class="text-sm">
-                Certilytics' predictive insights support health & wellness programs for dozens of large,
-                self-funded employers. A retrospective analysis shows these programs save participating
-                employers an average of over $200 PMPY while improving employee health.
+                <?php the_field('case_study_description')?>
             </p>
-            <div>see how we did it - <b>Download our case study now!</b></div>
+            <div><?php the_field('download_form_modal_code')?></div>
         </div>
         <div class="mx-auto case__study__form w-full md:w-6/12 lg:w-5/12 flex flex-col items-center mt-10 lg:mt-0">
-            <div class=" h-40 w-32 bg-dark-background"></div>
+            <div class="w-full lg:w-1/2"><img src="<?php the_field('form_image')?>" alt=""></div>
+            <?php echo FrmFormsController::get_form_shortcode(array('id' => get_field('download_brochure_form_id'), 'title' => false, 'description' => true)) ?>
         </div>
     </div>
 </section>
