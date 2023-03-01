a2414c14aacbcc93fce95d4e77b4b08890ca2b41
Oswaldo Rangel <lebial@gmail.com>
2023-03-01 09:59:41 -0600

    fixed feedback (#94)

--- components/about-us/au-story.php
+++ components/about-us/au-story.php
@@ -12,16 +12,10 @@ function render_cards()
 ?>
 <section class="au__story w-full">
   <div class="w-full px-8 md:px-16 lg:px-0 text-left lg:text-center my-12 lg:my-24 relative">
-    <h2 class="text-[#5E5E5E] text-4xl lg:text-6xl font-bold lg:font-light pb-0 lg:pb-8 text-center"><?php the_field('story_title') ?></h2>
+    <h2 class=" text-gray-header text-4xl lg:text-6xl font-bold lg:font-light pb-0 lg:pb-8 text-center"><?php the_field('story_title') ?></h2>
     <div class="au__story__description w-full lg:w-[84%] 2xl:w-7/12 lg:mx-auto">
       <p class="text-[#5E5E5E] max-w-none text-lg lg:text-xl 2xl:text-2xl">
-        We launched in 2014 with three employees and two fundamental customers. Today we have over 120 employees, dozens
-        of customers, and 250M+ patient lives on platform. How'd we do it? Simple. We delivered value
-        from day one. We were driven by our frustration with existing analytic tools and belief that we could
-        do better.</br></br>
-        Our team includes data scientists, actuaries, clinicians, developers, and data engineers whose
-        combined expertise has transformed our customers' businesses for the age of AI.
-        <?php //the_field('story_description')
+        <?php the_field('story_description')
         ?>
       </p>
     </div>
