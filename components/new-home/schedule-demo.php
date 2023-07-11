607fb325f94c11a9adf18770be16c88a3e2b0684
Oswaldo Rangel <lebial@gmail.com>
2023-07-11 18:55:21 -0500

    Fix/schedule demo fixes (#147)
    * fixed feecback and added new background dark color
    
    * fixed contact us section spacing on all pages
    
    * fixed merge conflicts

--- components/new-home/schedule-demo.php
+++ components/new-home/schedule-demo.php
@@ -1,5 +1,5 @@
 <section class="min-h-[60vh] 2xl:min-h-[45vh]">
-  <div class="contact__us__image__container w-full min-h-[60vh] 2xl:min-h-[45vh] relative text-center flex justify-end items-center bg-dark-blue-background">
+  <div class="contact__us__image__container w-full min-h-[60vh] 2xl:min-h-[45vh] relative text-center flex justify-end items-center">
     <img src="<?php the_field('ofast_image') ?>" class="w-full absolute top-0 left-0 z-[-1] h-full object-cover" alt="">
     <div class="action__quote__container w-full lg:w-6/12" data-aos="fade-up">
       <div class="w-full flex flex-col items-center px-4 md:px-32 lg:px-0 lg:pr-20 pt-4 lg:pt-0">
@@ -12,9 +12,9 @@
 			hover:bg-primary text-xs lg:text-base " type="button">Schedule
             Demo</button>
         </div>
-        <div class="w-full lg:w-10/12 2xl:w-7/12">
+        <div class="w-full">
           <!-- <h3 class="action__quote__container__text text-3xl 2xl:text-5xl text-white font-bold text-center mb-0 mt-8"><?php the_field('schedule_demo_title') ?></h3> -->
-          <p class=" text-2xl text-white font-bold text-center mb-0">
+          <p class=" text-2xl text-white font-bold text-center mb-8">
             Receive a brief introduction or dive deep into our solutions by choosing a 15, 30 or 60 minute demo
           </p>
         </div>
