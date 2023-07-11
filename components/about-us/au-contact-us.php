3d9aa1e3ec1380dd23fe8e8ced0b65a30abb65d6
Oswaldo Rangel <lebial@gmail.com>
2023-07-11 17:42:54 -0600

    fixed feecback and added new background dark color

--- components/about-us/au-contact-us.php
+++ components/about-us/au-contact-us.php
@@ -1,11 +1,21 @@
-<section class="aboutUs__contact__us lg:min-h-[50vh]">
-  <div class="contact__us__image__container w-full lg:min-h-[50vh] relative text-center flex justify-end items-center">
-    <img src="<?php the_field('contact_image') ?>" class="w-full absolute top-0 left-0 z-[-1] h-full object-cover" alt="contact Us bacground">
+<section class="min-h-[60vh] 2xl:min-h-[45vh]">
+  <div class="contact__us__image__container w-full min-h-[60vh] 2xl:min-h-[45vh] relative text-center flex justify-end items-center bg-dark-blue-background">
+    <img src="<?php the_field('contact_image') ?>" class="w-full absolute top-0 left-0 z-[-1] h-full object-cover" alt="">
     <div class="action__quote__container w-full lg:w-6/12" data-aos="fade-up">
-      <div class="w-full flex flex-col items-center px-4 md:px-32 lg:px-0 lg:pr-20 pt-16 lg:pt-0">
-        <h3 class="action__quote__container__text text-3xl 2xl:text-5xl text-white font-bold text-center mb-0 mt-8"><?php the_field('contact_us_title') ?></h3>
-        <div class="action__quote__container__button w-full mx-auto mt-10 pb-14 flex lg:px-1 xl:px-2 justify-center">
-          <a target="_blank" rel="noopener noreferrer" href="<?php the_field('schedule_demo_link') ?>" class=" w-fit bg-primary text-white font-bold rounded-lg text-xl text-center py-3 px-5 "><?php the_field('schedule_demo_text') ?></a>
+      <div class="w-full flex flex-col items-center px-4 md:px-32 lg:px-0 lg:pr-20 pt-4 lg:pt-0">
+        <div class="w-full lg:w-10/12 2xl:w-7/12">
+          <h3 class="action__quote__container__text text-3xl 2xl:text-5xl text-white font-bold text-center mb-0 mt-8"><?php the_field('contact_us_title') ?></h3>
+        </div>
+        <div class="action__quote__container__button w-full mx-auto mt-10 pb-10 flex lg:px-1 xl:px-2 justify-center">
+          <button class="schedule__demo__button px-3 py-2  border-solid border-primary border rounded-3xl
+			text-white font-bold hover:cursor-pointer transition-all duration-300
+			hover:bg-primary text-xs lg:text-base " type="button">Schedule
+            Demo</button>
+        </div>
+        <div class="w-full lg:w-10/12 2xl:w-7/12">
+          <p class=" text-2xl text-white font-bold text-center mb-0">
+            Receive a brief introduction or dive deep into our solutions by choosing a 15, 30 or 60 minute demo
+          </p>
         </div>
       </div>
     </div>
--- components/new-home/insights.php
+++ components/new-home/insights.php
@@ -9,11 +9,11 @@ function render_new_cards()
         echo '<img class="rounded-t-lg md:h-30 lg:h-44 2xl:h-52 w-full object-cover object-center blur-[2px] hover:blur-none" src="' . get_field('post_hero_image', $tmp_post_id) . '" alt="">';
         echo '<div class="insights__card__text md:h-64 lg:h-56 2xl:h-64 flex flex-col justify-between p-4 custom-shadow rounded-b-lg">';
         echo '<div class="line-clamp overflow-hidden">';
-        echo '<h3 class="text-base md:text-xl 2xl:text-xl lg:leading-5 2xl:leading-6 font-semibold mb-3 mt-2 text-gray-header">' . get_the_title($tmp_post_id) . '</h3>';
-        echo '<div class="mb-0 w-full"><p class="line-clamp-2">' . strip_tags(get_field('post_content', $tmp_post_id)[0]['post_text']) . '</p></div>';
+        echo '<h3 class="text-base md:text-xl 2xl:text-xl lg:leading-5 2xl:leading-6 font-semibold mb-3 mt-2 text-dark-blue-background">' . get_the_title($tmp_post_id) . '</h3>';
+        echo '<div class="mb-0 w-full"><p class="line-clamp-2" style="color: #180f36;">' . strip_tags(get_field('post_content', $tmp_post_id)[0]['post_text']) . '</p></div>';
         echo '</div>';
         echo '<div class="flex items-center flex-wrap pt-4 pb-3 ">';
-        echo '<a href="' . get_permalink($tmp_post_id) . '" class="text-gray-header font-semibold cursor-pointer inline-flex items-center md:mb-2 lg:mb-0 underline underline-offset-1  hover:text-primary transition duration-300 ease-in text-xs md:text-sm 2xl:text-base">Read the Article
+        echo '<a href="' . get_permalink($tmp_post_id) . '" class=" text-dark-blue-background font-semibold cursor-pointer inline-flex items-center md:mb-2 lg:mb-0 underline underline-offset-1  hover:text-primary transition duration-300 ease-in text-xs md:text-sm 2xl:text-base">Read the Article
                                     <svg class="w-3 h-3 md:w-4 md:h-4 ml-1" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
                                         fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path d="M12 5l7 7-7 7"></path>
@@ -30,8 +30,8 @@ function render_new_cards()
 <section class="home__insights__section w-full h-full flex items-center">
     <div class="container px-5 pb-20 lg:pb-28 2xl:pb-32 pt-16 mx-auto">
         <div class="text-center mb-4 md:mb-8 lg:mb-14">
-            <h3 class="hidden lg:inline reveal-text text font-semibold text-2xl lg:text-4xl text-center text-gray-header">Certilytics News & Expert Insights</h5>
-                <h3 class="lg:hidden reveal-text text font-semibold text-2xl md:text-3xl lg:text-4xl text-center text-gray-header">Certilytics News & <br> Expert Insights</h5>
+            <h3 class="hidden lg:inline reveal-text text font-semibold text-2xl lg:text-4xl text-center text-dark-blue-background">Certilytics News & Expert Insights</h5>
+                <h3 class="lg:hidden reveal-text text font-semibold text-2xl md:text-3xl lg:text-4xl text-center text-dark-blue-background">Certilytics News & <br> Expert Insights</h5>
         </div>
         <div class="hidden lg:flex flex-wrap -m-4 w-10/12 mx-auto">
             <?php render_new_cards() ?>
