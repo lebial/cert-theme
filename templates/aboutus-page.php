df172092e26a9fadada082cf914cfbe2c93f747c
Oswaldo Rangel <lebial@gmail.com>
2023-07-12 14:28:52 -0600

    fixed schedule demo modal sizing

--- templates/aboutus-page.php
+++ templates/aboutus-page.php
@@ -9,6 +9,7 @@
 <?php get_template_part('components/about-us/au-solutions') ?>
 <?php get_template_part('components/about-us/au-our-team') ?>
 <?php get_template_part('components/about-us/au-careers') ?>
-<?php get_template_part('components/about-us/au-story-slider') ?>
+<?php //get_template_part('components/about-us/au-story-slider') 
+?>
 <?php get_template_part('components/about-us/au-contact-us') ?>
 <?php get_footer() ?>
