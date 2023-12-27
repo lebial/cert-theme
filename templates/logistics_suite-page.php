c370c7b61a3bfc04877fcf08035d46f9a450c0ff
Rogelio Oseguera de la Cruz <rogeliooseguera16@gmail.com>
2023-12-27 00:38:21 -0600

    Add logistics suite page

---
+++ templates/logistics_suite-page.php
@@ -0,0 +1,12 @@
+<?php
+// Template Name: Logistics Page
+?>
+
+<?php get_header(); ?>
+<?php get_template_part('components/logistics-suite/hero-logistics') ?>
+<?php get_template_part('components/new-platform/highlight-video') ?>
+<?php get_template_part('components/logistics-suite/data-processing') ?>
+<?php get_template_part('components/logistics-suite/security-framework') ?>
+<?php get_template_part('components/platform/contact-us') ?>
+<?php get_footer(); ?>
+
