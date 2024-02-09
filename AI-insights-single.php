a38b0f40e9b62e349425758e4cab1377a09ff390
Rogelio Oseguera de la Cruz <rogeliooseguera16@gmail.com>
2024-02-09 12:13:34 -0600

    Add x icon to insights post

--- AI-insights-single.php
+++ AI-insights-single.php
@@ -184,79 +184,83 @@ function get_latest_posts($number_of_posts = 3) {
 
 ?>
 <?php get_header(); ?>
-<main class="post__page w-full">
-    <section class="article__body w-full flex flex-col lg:flex-row px-5 ">
-        <article class="w-full text-dark-blue-background lg:w-auto lg:flex-1 flex justify-center px-5 lg:px-10 pr-4 lg:pr-24 ">
-            <div class="content__container post__dynamic__content w-full flex flex-col items-center lg:px-10">
-                <div class="post__content__container w-full order-2 lg:-order-1 mb-12">
-                    <div class="pt-12 pb-8">
-                        <p class="pb-4 text-dark-blue-background">AI Insights by Certilytics</p>
-                        <?php render_tags() ?>
+<div class="w-full flex justify-center">
+    <div class="max-w-screen-2xl flex flex-col">
+        <main class="post__page w-full">
+            <section class="article__body w-full flex flex-col lg:flex-row px-5 ">
+                <article class="w-full text-dark-blue-background lg:w-auto lg:flex-1 flex justify-center px-5 lg:px-10 pr-4 lg:pr-24 ">
+                    <div class="content__container post__dynamic__content w-full flex flex-col items-center lg:px-10">
+                        <div class="post__content__container w-full order-2 lg:-order-1 mb-12">
+                            <div class="pt-12 pb-8">
+                                <p class="pb-4 text-dark-blue-background">AI Insights by Certilytics</p>
+                                <?php render_tags() ?>
+                            </div>
+                            <h1 class="!text-dark-blue-background min-w-fit tracking-normal font-bold text-center mb-4 lg:text-left mt-6 lg:mt-0" style="font-size: calc(2rem + (1vw - 3.2px) * 1.9375);"><?php the_title() ?></h1>
+                            <h2 class="!text-dark-blue-background min-w-fit tracking-normal font-normal text-center lg:text-left mt-6 lg:mt-0" style="font-size: calc(0.5rem + (1vw - 3.2px) * 1.9375);"><?php the_field('post_h2') ?></h2>
+                            <div class="post__author__container mt-8 flex justify-center lg:justify-start">
+                                <div class="user__container flex">
+                                    <img src="<?php echo $tmp_author['author_avatar'] ?>" alt="user avatar" class="rounded-full object-contain mr-4 w-16 h-16 lg:w-24 lg:h-24">
+                                    <div class="author__name__container w-full flex flex-col justify-center relative">
+                                        <p class="font-normal mb-0">By <?php echo $tmp_author['author_name'] ?></p>
+                                        <p class=""><?php echo $tmp_author['publication_date'] ?></p>
+                                        <?php render_extra_links($tmp_author['extra_author_links']) ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex items-center mt-8">
+                                <p>SHARE IT : &nbsp;</p>
+                                <a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo the_permalink(); ?>&amp;isFramed=true&amp;lang=en_US&amp;xd_origin_host=<?php echo the_permalink(); ?>">
+                                    <img class="w-7 mr-2" src="<?php echo get_template_directory_uri() ?>/dist/assets/images/postsPage/Linkenin-Navy.jpg" alt="">
+                                </a>
+                                <a href="https://twitter.com/share?url=<?php echo the_permalink() ?>&text=<?php echo strip_tags(the_title()) ?>">
+                                    <img class="w-7 mx-2" src="<?php echo get_template_directory_uri() ?>/dist/assets/images/postsPage/X-icon-navy.jpg" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="post__article__container">
+                            <?php render_dynamic_content() ?>
+                        </div>
                     </div>
-                    <h1 class="!text-dark-blue-background min-w-fit tracking-normal font-bold text-center mb-4 lg:text-left mt-6 lg:mt-0" style="font-size: calc(2rem + (1vw - 3.2px) * 1.9375);"><?php the_title() ?></h1>
-                    <h2 class="!text-dark-blue-background min-w-fit tracking-normal font-normal text-center lg:text-left mt-6 lg:mt-0" style="font-size: calc(0.5rem + (1vw - 3.2px) * 1.9375);"><?php the_field('post_h2') ?></h2>
-                    <div class="post__author__container mt-8 flex justify-center lg:justify-start">
-                        <div class="user__container flex">
-                            <img src="<?php echo $tmp_author['author_avatar'] ?>" alt="user avatar" class="rounded-full object-contain mr-4 w-16 h-16 lg:w-24 lg:h-24">
-                            <div class="author__name__container w-full flex flex-col justify-center relative">
-                                <p class="font-normal mb-0">By <?php echo $tmp_author['author_name'] ?></p>
-                                <p class=""><?php echo $tmp_author['publication_date'] ?></p>
-                                <?php render_extra_links($tmp_author['extra_author_links']) ?>
+                </article>
+
+                <aside class="sticky w-[320px] top-[15%] h-fit hidden lg:block">
+                    <div class="hidden <?php echo $hide_class ?>">
+                        <?php render_subscribe_form() ?>
+                    </div>
+                    <div class="flex-1 h-0 border-t-[1px] mt-8 border-gray-400/50 border-solid"></div>
+                    <div class="related__posts__container w-full <?php echo $remove_mt_if_shown ?>">
+                        <div class="related__posts__headline w-full mb-4">
+                            <p class="w-fit mr-4 font-bold">Content </p>
+                            <div class="content__option__container w-full flex flex-col">
+                                <?php render_content_navigation() ?>
                             </div>
                         </div>
                     </div>
-                    <div class="flex items-center mt-8">
-                        <p>SHARE IT : &nbsp;</p>
-                        <a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo the_permalink(); ?>&amp;isFramed=true&amp;lang=en_US&amp;xd_origin_host=<?php echo the_permalink(); ?>">
-                            <img class="w-7 mr-2" src="<?php echo get_template_directory_uri() ?>/dist/assets/images/postsPage/Linkenin-Navy.jpg" alt="">
-                        </a>
-                        <a href="https://twitter.com/share?url=<?php echo the_permalink() ?>&text=<?php echo strip_tags(the_title()) ?>">
-                            <img class="w-7 mx-2" src="<?php echo get_template_directory_uri() ?>/dist/assets/images/postsPage/Twitter-navy.jpg" alt="">
-                        </a>
-                    </div>
-                </div>
-                <div class="post__article__container">
-                    <?php render_dynamic_content() ?>
+                </aside>
+            </section>
+
+            <div class="mobile__related__posts w-full mt-8 lg:hidden flex flex-col items-center">
+                <p class="text-gray-header font-bold text-center text-2xl">You Might Also Like</p>
+                <div class="w-full flex flex-col items-center px-6 mt-8">
+                    <?php render_mobile_related_posts() ?>
                 </div>
             </div>
-        </article>
 
-        <aside class="sticky w-[320px] top-[15%] h-fit hidden lg:block">
-            <div class="hidden <?php echo $hide_class ?>">
-                <?php render_subscribe_form() ?>
-            </div>
-            <div class="flex-1 h-0 border-t-[1px] mt-8 border-gray-400/50 border-solid"></div>
-            <div class="related__posts__container w-full <?php echo $remove_mt_if_shown ?>">
-                <div class="related__posts__headline w-full mb-4">
-                    <p class="w-fit mr-4 font-bold">Content </p>
-                    <div class="content__option__container w-full flex flex-col">
-                        <?php render_content_navigation() ?>
-                    </div>
+            <div class="mobile__subscribe__to__blog w-full lg:hidden block">
+                <div class="subscribe__form__container w-full bg-dark-blue-background  px-5 flex flex-col items-center justify-center">
+                    <p class="text-white mt-10 mb-8">Sign Up for AI Insights by Email</p>
+                    <?php echo FrmFormsController::get_form_shortcode(array('id' => get_field('subscribe_form_id'), 'title' => false, 'description' => false)); ?>
                 </div>
             </div>
-        </aside>
-    </section>
+        </main>
 
-    <div class="mobile__related__posts w-full mt-8 lg:hidden flex flex-col items-center">
-        <p class="text-gray-header font-bold text-center text-2xl">You Might Also Like</p>
-        <div class="w-full flex flex-col items-center px-6 mt-8">
-            <?php render_mobile_related_posts() ?>
-        </div>
-    </div>
-
-    <div class="mobile__subscribe__to__blog w-full lg:hidden block">
-        <div class="subscribe__form__container w-full bg-dark-blue-background  px-5 flex flex-col items-center justify-center">
-            <p class="text-white mt-10 mb-8">Sign Up for AI Insights by Email</p>
-            <?php echo FrmFormsController::get_form_shortcode(array('id' => get_field('subscribe_form_id'), 'title' => false, 'description' => false)); ?>
-        </div>
-    </div>
-</main>
-
-<div class="w-full">
-    <div class="w-11/12 mx-auto py-12">
-        <h3 class="text-dark-blue-background ml-12 pb-4">Related Insights</h3>
-        <div class="flex justify-between space-x-8 px-12 pb-12">
-            <?php get_latest_posts()?>
+        <div class="w-full">
+            <div class="w-11/12 mx-auto py-12">
+                <h3 class="text-dark-blue-background ml-12 pb-4">Related Insights</h3>
+                <div class="flex justify-between space-x-8 px-12 pb-12">
+                    <?php get_latest_posts()?>
+                </div>
+            </div>
         </div>
     </div>
 </div>
