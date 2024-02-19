7378a73dd651860ad4c2961a116166297aa81445
Rogelio Oseguera de la Cruz <rogeliooseguera16@gmail.com>
2024-02-19 14:31:41 -0600

    Add mobile version to ai insights page

--- AI-insights-single.php
+++ AI-insights-single.php
@@ -11,25 +11,6 @@ $hide_class = $show_content_navigation ? 'lg:block' : '';
 $hide_alternate = $show_content_navigation ? '' : 'lg:block';
 $remove_mt_if_shown = $show_content_navigation ? 'mt-4' : 'mt-0';
 
-function render_mobile_related_posts()
-{
-    $related_posts = get_field('related_posts');
-    if (!$related_posts)  $related_posts = get_realted_posts_by_tags();
-    foreach ($related_posts as $tmp_post_id) {
-        echo '<div class="mb-8">';
-        echo '<div class="related__post__body mt-4 shadow-xl max-w-sm">';
-        echo '<div class=" w-full bg-dark-blue-background mr-4 aspect-video">
-								<img src="' . get_field('post_hero_image', $tmp_post_id) . '" alt="_" class="w-full h-full">
-						</div>';
-        echo '<div class="post__info w-full p-8">';
-        echo '<p class="post__title text-sm font-bold" style="margin-bottom: 20px;">' . get_the_title($tmp_post_id) . '</p>';
-        echo '<a href="' . get_permalink($tmp_post_id) . '" class="bg-primary text-white rounded-md px-4 py-2 text-sm font-bold ">Read More</a>';
-        echo '</div>';
-        echo '</div>';
-        echo '</div>';
-    }
-}
-
 function render_text_content($component)
 {
     echo '<div class="wysiwyg mb-14">';
@@ -55,7 +36,7 @@ function render_image_container($component)
 function render_quotes($component)
 {
     $quotes = $component['post_quotes'];
-    echo '<div class="w-full post__quotes__slider mx-8 py-6">';
+    echo '<div class="w-full post__quotes__slider mx-8 py-6 hidden">';
     foreach ($quotes as $quote) {
         echo '<div class="quote__body flex flex-col items-center">';
         echo '<p class="font-bold text-lg text-gray-header max-w-sm text-center" style="max-width: 700px;">';
@@ -130,7 +111,7 @@ function render_tags() {
         foreach( $post_tags as $tag ) {
             echo'
                 <button class="px-5 py-1 border-solid border-primary border lg:border-[2px] rounded-3xl
-                text-dark-blue-background hover:text-white mr-3 font-medium cursor-default hover:cursor-default transition-all duration-300
+                text-dark-blue-background hover:text-white mr-2 lg:mr-3 mb-3 font-medium cursor-default hover:cursor-default transition-all duration-300
                 hover:bg-primary text-xs lg:text-sm " type="button" name="Example">'. esc_html($tag->name) .'
                 </button>
             ';
@@ -156,14 +137,14 @@ function get_latest_posts($number_of_posts = 3) {
             $latest_posts_query->the_post(); // Set the current post in the $post object.
             $post_content = limit_post_text(strip_tags(get_field('post_content')[0]['post_text']),85,'...');
             echo '
-                <div class="max-w-sm bg-white rounded-lg">
+                <div class="max-w-sm px-4 lg:px-0 bg-white rounded-lg pb-12 lg:pb-0">
                     <div class="h-52 w-full overflow-hidden rounded-lg">
                         <img class="w-full h-full object-cover" src="'.get_field('post_hero_image').'" alt="" />
                     </div>
                     <div class="py-2 flex flex-col justify-between h-56">
                         <span class="font-base text-base text-gray-400">'.get_the_date('F j, Y').'</span>
-                        <h5 class="pb-2 text-xl font-bold tracking-tight text-gray-900 line-clamp-2">'.get_the_title().'</h5>
-                        <p class="pb-6 font-base text-base text-dark-blue-background">'.esc_html($post_content).'</p>
+                        <h5 class="text-xl font-bold tracking-tight text-gray-900 line-clamp-2">'.get_the_title().'</h5>
+                        <p class="pt-2 pb-6 font-base text-base text-dark-blue-background">'.esc_html($post_content).'</p>
                         <div class="">
                             <a href="'.get_permalink().'" target="_blank" class="schedule__demo__button nav__schedule__demo__button px-3 py-2 border-solid border-primary border lg:border-[2px] rounded-3xl
                             text-primary hover:text-white mr-3 font-medium hover:cursor-pointer transition-all duration-300
@@ -190,8 +171,8 @@ function get_latest_posts($number_of_posts = 3) {
             <section class="article__body w-full flex flex-col lg:flex-row px-5 ">
                 <article class="w-full text-dark-blue-background lg:w-auto lg:flex-1 flex justify-center px-5 lg:px-10 pr-4 lg:pr-20 ">
                     <div class="content__container post__dynamic__content w-full flex flex-col items-center lg:px-10">
-                        <div class="post__content__container w-full order-2 lg:-order-1 mb-12">
-                            <div class="pt-12 pb-8">
+                        <div class="post__content__container w-full mb-12">
+                            <div class="pt-8 pb-2 lg:pt-12 lg:pb-8">
                                 <p class="pb-4 text-dark-blue-background">AI Insights by Certilytics</p>
                                 <?php render_tags() ?>
                             </div>
@@ -207,7 +188,7 @@ function get_latest_posts($number_of_posts = 3) {
                                     </div>
                                 </div>
                             </div>
-                            <div class="flex items-center mt-8">
+                            <div class="flex items-center justify-center lg:justify-start mt-8">
                                 <p>SHARE IT : &nbsp;</p>
                                 <a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo the_permalink(); ?>&amp;isFramed=true&amp;lang=en_US&amp;xd_origin_host=<?php echo the_permalink(); ?>" target="_blank">
                                     <img class="w-7 mr-2" src="<?php echo get_template_directory_uri() ?>/dist/assets/images/postsPage/Linkenin-Navy.jpg" alt="">
@@ -238,11 +219,13 @@ function get_latest_posts($number_of_posts = 3) {
                     </div>
                 </aside>
             </section>
+        </main>
 
-            <div class="mobile__related__posts w-full mt-8 lg:hidden flex flex-col items-center">
-                <p class="text-gray-header font-bold text-center text-2xl">You Might Also Like</p>
-                <div class="w-full flex flex-col items-center px-6 mt-8">
-                    <?php render_mobile_related_posts() ?>
+        <div class="w-full flex flex-col items-center lg:items-start lg:flex-row">
+            <div class="w-11/12 lg:px-10 pt-8 lg:pt-12 lg:pb-12">
+                <h3 class="text-dark-blue-background ml-0 lg:ml-12 pb-8 lg:pb-4 text-center lg:text-left text-2xl">Related Insights</h3>
+                <div class="flex flex-col lg:flex-row lg:justify-between lg:space-x-8 lg:px-12 lg:pb-12">
+                    <?php get_latest_posts()?>
                 </div>
             </div>
 
@@ -252,16 +235,9 @@ function get_latest_posts($number_of_posts = 3) {
                     <?php echo FrmFormsController::get_form_shortcode(array('id' => get_field('subscribe_form_id'), 'title' => false, 'description' => false)); ?>
                 </div>
             </div>
-        </main>
-
-        <div class="w-full">
-            <div class="w-11/12 lg:px-10 py-12">
-                <h3 class="text-dark-blue-background ml-12 pb-4">Related Insights</h3>
-                <div class="flex justify-between space-x-8 px-12 pb-12">
-                    <?php get_latest_posts()?>
-                </div>
-            </div>
         </div>
+
+
     </div>
 </div>
 
