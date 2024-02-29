065011b1e3b44a605620aa685b2786a51042fefd
Oswaldo Rangel <lebial@gmail.com>
2024-02-29 09:18:39 -0700

    fixed feedback and uploaded videos to cdn (#223)

--- footer.php
+++ footer.php
@@ -50,8 +50,9 @@ function render_link_groups()
 
 	$link_groups = get_field('footer_link_groups', 'option') ? get_field('footer_link_groups', 'options') : $data_mock;
 
-	foreach ($link_groups as $link_group) {
-		echo '<div class="footer__links lg:flex flex-col items-center lg:items-start flex-1 pr-6">';
+	foreach ($link_groups as $idx => $link_group) {
+		$pr_class = $idx == 0 ? '' : 'pr-6';
+		echo '<div class="footer__links lg:flex flex-col items-center lg:items-start flex-1 '.$pr_class.' ">';
 		echo '<div class="mb-4 flex justify-between w-auto whitespace-nowrap">';
 		if ($link_group['button_link']) {
 			$open_tab = $link_group['button_text'] == 'Contact Us' ? 'target="_blank"' : '';
