09deb14b29b95c515e26e63dc6c1a28cb4696ee5
Oswaldo Rangel <lebial@gmail.com>
2024-03-20 14:33:21 -0600

    fixed news insights feedback (#241)

--- single_new_version.php
+++ single_new_version.php
@@ -22,7 +22,7 @@ function render_related_posts()
 							<img src="' . get_field('post_hero_image', $tmp_post_id) . '" alt="" class="w-full h-full">
 					</div>';
         echo '<div class="post__info flex-1">';
-        echo '<p class="post__title mb-0 font-bold leading-4" style="font-size: 12px;">' . get_the_title($tmp_post_id) . '</p>';
+        echo '<p class="post__title mb-0 font-bold leading-4 text-dark-blue-background" style="font-size: 12px;">' . get_the_title($tmp_post_id) . '</p>';
         echo '</div>';
         echo '</div>';
         echo '</a>';
@@ -35,13 +35,13 @@ function render_mobile_related_posts()
     if (!$related_posts)  $related_posts = get_realted_posts_by_tags();
     foreach ($related_posts as $tmp_post_id) {
         echo '<div class="mb-8">';
-        echo '<div class="related__post__body mt-4 shadow-xl max-w-sm">';
-        echo '<div class=" w-full bg-dark-blue-background mr-4 aspect-video">
+        echo '<div class="related__post__body mt-4 shadow-xl max-w-sm rounded-lg overflow-hidden">';
+        echo '<div class=" w-full bg-dark-blue-background mr-4 aspect-video rounded-md">
 								<img src="' . get_field('post_hero_image', $tmp_post_id) . '" alt="_" class="w-full h-full">
 						</div>';
         echo '<div class="post__info w-full p-8">';
         echo '<p class="post__title text-sm font-bold" style="margin-bottom: 20px;">' . get_the_title($tmp_post_id) . '</p>';
-        echo '<a href="' . get_permalink($tmp_post_id) . '" class="bg-primary text-white rounded-md px-4 py-2 text-sm font-bold ">Read More</a>';
+        echo '<a href="' . get_permalink($tmp_post_id) . '" class="hover:bg-primary transition-all duration-300 bg-transparent text-dark-blue-background  hover:text-white rounded-3xl px-4 py-2 text-sm font-bold border-solid border border-primary">Read More</a>';
         echo '</div>';
         echo '</div>';
         echo '</div>';
@@ -50,7 +50,7 @@ function render_mobile_related_posts()
 
 function render_text_content($component)
 {
-    echo '<div class="wysiwyg mb-14">';
+    echo '<div class="wysiwyg mb-14 text-dark-blue-background">';
     echo $component['post_text'];
     echo '</div>';
 }
@@ -125,7 +125,7 @@ function render_content_navigation()
 {
     $navigation = get_field('content_navigation');
     foreach ($navigation as $nav_option) {
-        echo '<a href="#' . $nav_option['anchor_id'] . '" class="post__navigation__option w-full pl-4 py-1 mb-2 font-medium text-gray-header ">' . $nav_option['display_text'] . '</a>';
+        echo '<a href="#' . $nav_option['anchor_id'] . '" class="post__navigation__option w-full pl-4 py-1 mb-2 font-medium text-dark-blue-background">' . $nav_option['display_text'] . '</a>';
     }
 }
 
@@ -174,10 +174,10 @@ function render_extra_links($links, $color = 'white')
         </div>
     </section>
     <section class="post__description w-full pt-20 mb-14">
-        <p class="text-center text-gray-400 mx-auto"><?php echo get_the_date('F j, Y') ?></p>
-        <div class="mt-10 flex">
+        <p class="text-center text-dark-blue-background mx-auto"><?php echo get_the_date('F j, Y') ?></p>
+        <div class="mt-10 flex pl-5 lg:pl-0">
             <div class="lg:w-[280px]"></div>
-            <h2 class=" flex-1 text-center text-2xl lg:text-4xl text-gray-header px-8 lg:px-20 tracking-normal">
+            <h2 class=" flex-1 text-left lg:text-center text-2xl lg:text-4xl text-dark-blue-background px-5 lg:px-20 tracking-normal">
                 <?php the_field('post_h2') ?>
             </h2>
             <div class="lg:w-[280px]"></div>
@@ -189,13 +189,13 @@ function render_extra_links($links, $color = 'white')
                 <?php render_subscribe_form() ?>
             </div>
             <div class="hidden <?php echo $hide_class ?> ">
-                <p class="font-bold">Contents</p>
+                <p class="font-bold text-dark-blue-background">Contents</p>
                 <div class="content__option__container w-full flex flex-col">
                     <?php render_content_navigation() ?>
                 </div>
             </div>
-            <div class="flex items-center mt-5 border-t border-gray-header">
-                <p class="text-gray-header mr-4">SHARE IT :</p>
+            <div class="flex items-center mt-5 ml-5 lg:ml-0 border-t border-gray-header">
+                <p class=" text-dark-blue-background mr-4">SHARE IT :</p>
                 <a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo the_permalink(); ?>&amp;isFramed=true&amp;lang=en_US&amp;xd_origin_host=<?php echo the_permalink(); ?>">
                     <img class="w-7 mr-2" src="<?php echo get_template_directory_uri() ?>/dist/assets/images/postsPage/Linkenin-Navy.jpg" alt="">
                 </a>
@@ -218,7 +218,7 @@ function render_extra_links($links, $color = 'white')
             <!-- related posts container -->
             <div class="related__posts__container w-full <?php echo $remove_mt_if_shown ?>">
                 <div class="related__posts__headline flex w-full items-center mb-4">
-                    <p class="w-fit mr-4">You might also like </p>
+                    <p class="w-fit mr-4 text-dark-blue-background">You might also like </p>
                     <div class="flex-1 h-0 border-t-2 border-gray-400 border-solid"></div>
                 </div>
 
@@ -231,11 +231,11 @@ function render_extra_links($links, $color = 'white')
 
         </aside>
     </section>
-    <div class="w-full lg:w-7/12 border-b border-gray-header border-solid flex justify-center mb-14 mx-auto pb-6">
+    <div class="w-full lg:w-7/12 border-b border-gray-300 border-solid flex justify-center mb-14 mx-auto pb-6">
     </div>
 
     <div class="mobile__related__posts w-full mt-8 lg:hidden flex flex-col items-center">
-        <p class="text-gray-header font-bold text-center text-2xl">You Might Also Like</p>
+        <p class=" text-dark-blue-background font-bold text-center text-2xl">You Might Also Like</p>
         <div class="w-full flex flex-col items-center px-6 mt-8">
             <?php render_mobile_related_posts() ?>
         </div>
@@ -254,9 +254,9 @@ function render_extra_links($links, $color = 'white')
                 <img src="<?php echo $tmp_author['author_avatar'] ?>" alt="user avatar" class=" rounded-full w-24 h-24 object-contain" style="filter: grayscale(1);">
             </div>
             <div class="author__description">
-                <p class="mb-0 text-lg text-gray-header">ABOUT THE AUTHOR</p>
-                <p class="font-bold mb-4 text-3xl"><?php echo $tmp_author['author_name'] ?></p>
-                <p class=" text-base max-w-lg">
+                <p class="mb-0 text-lg text-dark-blue-background">ABOUT THE AUTHOR</p>
+                <p class="font-bold mb-4 text-3xl text-dark-blue-background"><?php echo $tmp_author['author_name'] ?></p>
+                <p class=" text-base max-w-lg text-dark-blue-background">
                     <?php echo strip_tags($tmp_author['author_description'], '<a><strong><br><span>') ?></p>
                 <div class="w-full flex flex-col">
                     <?php render_extra_links($tmp_author['extra_author_links'], 'black') ?>
