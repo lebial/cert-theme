254fe7704e23dc3dfd7691529342fdbd25dfc94b
Oswaldo Rangel <lebial@gmail.com>
2024-04-15 16:01:15 -0600

    finished load more on ai insights

--- components/ai-insights/highlighted-post.php
+++ components/ai-insights/highlighted-post.php
@@ -1,23 +1,29 @@
-<section class="ai__insights__highlighted__post w-full py-20">
-  <div class=" lg:pl-20">
+<?php
+$post = get_field('featured_post', 'options');
+$str_date = strtotime($post->post_date);
+$img = get_field('post_hero_image', $post->ID);
+$content = get_field('post_content', $post->ID);
+$custom_content = substr(strip_tags($content[0]['post_text']), 0, 140);
+$custom_content .= ',...';
+$link = get_permalink($post);
+?>
+<section class="ai__insights__highlighted__post w-full py-20 flex justify-center">
+  <div class="">
     <div class="highlighted__post__container flex">
       <div class="mr-10">
-        <img class="max-w-md rounded-xl" src="https://www.shutterstock.com/shutterstock/photos/2151833739/display_1500/stock-photo-portrait-of-a-young-latin-woman-with-pleasant-smile-and-crossed-arms-isolated-on-grey-wall-with-2151833739.jpg" alt="">
+        <img class="max-w-md rounded-xl" src="<?php echo $img['url']?>" alt="featured post image">
       </div>
       <div class=" w-8/12 max-w-md">
-        <p class=" text-gray-400 mb-2 text-base">May 3, 2021</p>
-        <p class="mb-2 font-bold text-dark-blue-background text-xl"> Fugium sit ut  rem lique sitas qui num vellatem unt et excea</p>
+        <p class=" text-gray-400 mb-2 text-base"><?php echo wp_date('M j, Y', $str_date)?></p>
+        <p class="mb-2 font-bold text-dark-blue-background text-xl"><?php echo $post->post_title ?></p>
         <p class="text-base max-w-lg">
-          Exerate ducia ium qui si blaut eicit, sent, officia sinus,
-          simagnat debis res dolori inverum facepudit, si conet
-          aut et offici di berum lam a quis aditatis sequias su-
-          sapides prepta que enet liam vollo moditasi re nitatur
+          <?php echo $custom_content ?>
         </p>
         <a class="border border-primary border-solid text-primary inline-block
                   rounded-3xl px-2 py-2 mt-2 !text-base
                    transition-all duration-300 hover:bg-primary hover:text-white
                   "
-                  href="#">Read Article</a>
+                  href="<?php echo $link ?>">Read Article</a>
       </div>
     </div>
   </div>
