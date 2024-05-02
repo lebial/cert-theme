ce3a2e652a2693edeee07e496de35a6473d81360
Oswaldo Rangel <lebial@gmail.com>
2024-05-02 09:52:49 -0600

    fixed social url on exec profile

---
+++ templates/resources-posts.php
@@ -0,0 +1,8 @@
+<?php
+// Template Name: Resources Posts
+// Template Post Type: post 
+?>
+
+<?php get_header() ?>
+<?php get_template_part('components/resources-posts/main-section') ?>
+<?php get_footer() ?>
