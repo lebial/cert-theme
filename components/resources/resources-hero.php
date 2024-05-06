1e764ed5da47cd055b862dca05b4266a24bb6994
Oswaldo Rangel <lebial@gmail.com>
2024-05-06 09:22:08 -0600

    Feature/resources details page (#278)
    * fixed social url on exec profile
    
    * finished resources details page

--- components/resources/resources-hero.php
+++ components/resources/resources-hero.php
@@ -4,8 +4,8 @@ function render_video_cards() {
   foreach ($cards as $card) {
     echo '
       <div class=" flex justify-center">
-        <div class="video__card flex w-9/12 mx-auto">
-          <div class="w-5/12">
+        <div class="video__card flex flex-col lg:flex-row w-9/12 lg:w-10/12 mx-auto">
+          <div class="w-full lg:w-5/12 ">
             <div class="w-full h-fit flex flex-col lg:justify-center relative">
               <div class="w-full h-full absolute flex justify-center items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 rounded-xl z-20">
                 <button class="second_video_play_button " style="background-color: transparent !important;">
@@ -19,7 +19,7 @@ function render_video_cards() {
               </video>
             </div>
           </div>
-          <div class="w-7/12 pl-8">
+          <div class="w-full lg:w-7/12 lg:pl-8 mt-4 lg:mt-0">
             <p class="uppercase text-gray-400 text-xs mb-1">Featured Video</p>
             <p class="text-white text-lg font-bold mb-1">Fugiam sit ut rem lique sitas qui num valletem  unt et excea</p>
             <p class="text-white text-xs pb-2">
@@ -37,7 +37,7 @@ function render_video_cards() {
 }
 ?>
 <section class="resources-hero bg-dark-blue-background py-14">
-  <div class="w-8/12 flex justify-center mx-auto">
+  <div class="w-full lg:w-8/12 flex justify-center mx-auto">
     <div class="resources__video__slider w-full">
       <?php render_video_cards()?>
     </div>
