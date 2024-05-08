53acb00b17936fb7e34df7fa80b64c9299bc969d
Oswaldo Rangel <lebial@gmail.com>
2024-05-08 09:16:44 -0600

    Feature/resources filtering (#280)
    * wip resources-filtering
    
    * wip resources filtering
    
    * fixed tags spacing
    
    * wip resources page
    
    * fixed resources-filtering

--- components/resources/resources-posts.php
+++ components/resources/resources-posts.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $icon = '
 <svg class="w-3 h-3 fill-dark-blue-background group-hover:fill-primary" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 185.343 185.343" xml:space="preserve">
   <g>
@@ -9,18 +9,14 @@ $icon = '
 </svg>
 ';
 
-function render_filter_options() {
-  $options = [
-    ['link' => 'articles', 'text' => 'Articles'],
-    ['link' => 'case-studies', 'text' => 'Case Studies'],
-    ['link' => 'videos', 'text' => 'Videos'],
-    ['link' => 'webinars', 'text' => 'Webinars'],
-  ];
+function render_filter_options()
+{
+  $options = get_field('filter_tags', get_option('page_for_posts'));
 
   foreach ($options as $option) {
     echo '
-      <a href="/resources/?tag='.$option['link'].'" class="text-dark-blue-background flex items-center group hover:text-primary mr-4 font-bold flex-[0_0_40%] lg:flex-auto justify-center mt-2 lg:mt-0">
-        '.$option['text'].' 
+      <a href="/resources/?tag=' . $option['url'] . '" class="text-dark-blue-background flex items-center group hover:text-primary mr-4 font-bold flex-[0_0_40%] lg:flex-none justify-center mt-2 lg:mt-0">
+        ' . $option['tag_name'] . ' 
         <svg class="w-3 h-3 fill-dark-blue-background group-hover:fill-primary" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 185.343 185.343" xml:space="preserve">
           <g>
             <g>
@@ -32,36 +28,79 @@ function render_filter_options() {
     ';
   }
 }
-function render_resources_cards() {
-if (have_posts()) : while (have_posts()) : the_post();
-  $content = get_field('post_content', get_the_ID());
+function render_articles()
+{
   $img = get_field('post_hero_image', get_the_ID());
   $tags = get_the_tags();
   echo '
     <div class="ai_insight_card rounded-lg mb-4 p-4 flex flex-col">
       <div class="ai_card_body shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] p-6 rounded-xl flex-1 flex flex-col">
-        <p class="text-gray-400 text-base mb-4 uppercase">'.$tags[0]->name.'</p>
-        <img src="'.$img.'" alt="post thumbnail" class=" rounded-lg my-4"/>
-        <h3 class=" text-dark-blue-background text-sm font-bold mb-2">'.get_the_title().'</h3>
+        <p class="text-gray-400 text-base mb-4 uppercase">' . $tags[0]->name . '</p>
+        <img src="' . $img . '" alt="post thumbnail" class=" rounded-lg my-4"/>
+        <h3 class=" text-dark-blue-background text-sm font-bold mb-2">' . get_the_title() . '</h3>
         <div class="flex-1 flex items-end">
-          <a href="'.get_the_permalink().'" class="py-1 px-2 border border-solid rounded-3xl border-primary text-primary text-xs inline-block mt-4 transition-all duration-300 hover:bg-primary hover:text-white">Read Article</a>
+          <a href="' . get_the_permalink() . '" class="py-1 px-2 border border-solid rounded-3xl border-primary text-primary text-xs inline-block mt-4 transition-all duration-300 hover:bg-primary hover:text-white">Read Article</a>
         </div>
       </div>
     </div>';
+}
 
-endwhile;
-endif; 
+function render_video_or_case_study()
+{
+  $post_type = get_field('post_type');
+  $keys = $post_type == 'video' ?
+    ['post_type' => 'video_post', 'image_key' => 'video_thumbnail']
+    : ['post_type' => 'case_study_post', 'image_key' => 'case_study_thumbnail'];
+  $post_fields = get_field($keys['post_type'], get_the_ID());
+  $img_url = $post_fields[$keys['image_key']];
+  $tags = get_the_tags();
+  echo '
+      <div class="resources_video_card rounded-lg mb-4 p-4 flex flex-col">
+        <div class="ai_card_body shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] p-6 rounded-xl flex-1 flex flex-col transition-all duration-300 hover:scale-105">
+          <p class="text-gray-400 text-xs mb-4 uppercase">' . $tags[0]->name . '</p>
+          <img src="' . $img_url . '" alt="post thumbnail" class=" rounded-lg my-4"/>
+          <h3 class=" text-dark-blue-background text-sm font-bold mb-2">' . get_the_title() . '</h3>
+          <div class="flex-1 flex items-end">
+            <a href="' . get_the_permalink() . '" class="py-1 px-2 border border-solid rounded-3xl border-primary text-primary text-xs inline-block mt-4 transition-all duration-300 hover:bg-primary hover:text-white">Read Article</a>
+          </div>
+        </div>
+      </div>';
+}
+
+function render_dynamic_content($tag_name)
+{
+  $components = [
+    'article' => 'render_articles',
+    'video' => 'render_video_or_case_study',
+    'case study' => 'render_video_or_case_study',
+  ];
+  $components[$tag_name]();
+}
+function render_resources_cards()
+{
+  $allowedTags = ['video' => 'video', 'case study' => 'case study', 'webinar' => 'webinar', 'article' => 'article'];
+  if (have_posts()):
+    while (have_posts()):
+      the_post();
+      $tags = get_the_tags();
+      $tag_name = $tags[0]->name;
+      if (isset($allowedTags[$tag_name])) {
+        render_dynamic_content($tag_name);
+      }
+
+    endwhile;
+  endif;
 }
 ?>
 
 <section class="resources__posts py-14">
   <div class="filter__options flex justify-center flex-wrap">
-    <?php render_filter_options()?>
+    <?php render_filter_options() ?>
   </div>
   <main class="mt-14">
     <div class="w-full max-w-screen-lg grid grid-cols-1 lg:grid-cols-3 gap-5 mx-auto auto-rows-fr" id="scrollContent">
-      <?php render_resources_cards()?>
+      <?php render_resources_cards() ?>
     </div>
-  <div class="w-full flex justify-center mb-20"><?php render_load_more('Resources')?></div>
+    <div class="w-full flex justify-center mb-20"><?php render_resources_load_more('Resources') ?></div>
   </main>
 </section>
