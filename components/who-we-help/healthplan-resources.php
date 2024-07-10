18bae7341b7eb6326bf8e33b2199519ff9c38b7f
Oswaldo Rangel <lebial@gmail.com>
2024-07-10 09:36:20 -0600

    Feature/who we help (#332)
    * wip who we help page
    
    * wip who we help page & aicpa logo mobile fix

---
+++ components/who-we-help/healthplan-resources.php
@@ -0,0 +1,35 @@
+<?php
+function render_video_or_case_study($post_id)
+{
+    $post_type = get_field('post_type', $post_id);
+    $keys = $post_type == 'video' ?
+        ['post_type' => 'video_post', 'image_key' => 'video_thumbnail']
+        : ['post_type' => 'case_study_post', 'image_key' => 'case_study_card_thumbnail'];
+    $post_fields = get_field($keys['post_type'], $post_id);
+    $img_url = $post_fields[$keys['image_key']];
+    $text = $post_type == 'video' ? 'Watch Video' : 'Read More';
+    echo '
+      <div class="resources_video_card rounded-lg mb-4 p-4 flex flex-col flex-1">
+        <div class="ai_card_body shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] p-6 rounded-xl flex-1 flex flex-col transition-all duration-300 hover:scale-105">
+          <img src="' . $img_url . '" alt="post thumbnail" class=" rounded-lg my-4"/>
+          <h3 class=" text-dark-blue-background text-sm font-bold mb-2">' . get_the_title($post_id) . '</h3>
+          <div class="flex-1 flex items-end">
+            <a href="' . get_the_permalink($post_id) . '" class="py-1 px-2 border border-solid rounded-3xl border-primary text-primary text-xs inline-block mt-4 transition-all duration-300 hover:bg-primary hover:text-white"">' . $text . '</a>
+          </div>
+        </div>
+      </div>';
+}
+function render_resources()
+{
+    $resources = get_field('health_plan_resources');
+    foreach ($resources as $resource) {
+        render_video_or_case_study($resource->ID);
+    }
+}
+?>
+<section class="health__plan__resources pb-16">
+    <h3 class=" text-dark-blue-background text-center py-16 mb-0"><?php the_field('resources_title') ?></h2>
+        <div class="resources__container mx-auto max-w-screen-lg flex">
+            <?php render_resources() ?>
+        </div>
+</section>
--- templates/who-we-help.php
+++ templates/who-we-help.php
@@ -7,5 +7,6 @@
 <?php get_template_part('components/who-we-help/help-hero') ?>
 <?php get_template_part('components/who-we-help/achievements') ?>
 <?php get_template_part('components/who-we-help/our-approach') ?>
+<?php get_template_part('components/who-we-help/healthplan-resources') ?>
 <?php get_template_part('components/platform/contact-us') ?>
 <?php get_footer(); ?>
