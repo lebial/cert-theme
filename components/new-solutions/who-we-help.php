9c6bff91c233f291fe08e1832e0b4f97cd8edf61
Oswaldo Rangel <lebial@gmail.com>
2024-07-19 16:40:29 -0600

    solutions page (#337)

---
+++ components/new-solutions/who-we-help.php
@@ -0,0 +1,33 @@
+<?php
+function render_who_we_help_cards()
+{
+
+    $cards = get_field('who_we_help_options');
+    foreach ($cards as $card) {
+        echo '<a class="flex justify-center" href="' . $card['link'] . '">
+            <div class="au__solutions__cards--card w-64 h-36 flex justify-center items-center m-4 relative overflow-hidden rounded-xl lg:transition lg:ease-in-out lg:delay-150 lg:hover:-translate-y-1 lg:hover:scale-105 lg:duration-300">
+              <img class="w-full h-full absolute top-0 left-0" src="' . $card['image'] . '" alt="' . $card['title'] . ' image">
+              <div class="relative flex items-center">
+                <p class="leading-snug">' . $card['title'] . '</p>
+                <div class="ml-1 w-8">
+                    <svg id="Layer_1" style="enable-background:new 0 0 64 64;" version="1.1" viewBox="0 0 64 64" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><g id="Icon-Chevron-Left" transform="translate(237.000000, 335.000000)"><polyline class=" fill-primary" id="Fill-35" points="-210.9,-289 -212.9,-291 -201.1,-302.7 -212.9,-314.4 -210.9,-316.4 -197.1,-302.7      -210.9,-289    "></polyline></g></g></svg>
+                </div>
+              </div>
+            </div>
+          </a>';
+    }
+}
+
+?>
+<section class="au__solutions w-full h-full py-16 lg:py-24 bg-indigo-50">
+    <h3 class="text-dark-blue-background text-2xl  xl:text-4xl text-center"><?php the_field('who_we_help_title') ?></h3>
+    <p class="text-dark-blue-background text-center mx-auto"><?php the_field('who_we_help_copy') ?></p>
+    <div class="w-full lg:w-11/12 flex flex-col items-center mx-auto">
+        <h3 class="font-bold text-2xl xl:text-4xl text-center block pb-8 mx-6 text-dark-blue-background reveal-text">
+            <?php the_field('solutions_title') ?>
+        </h3>
+        <div class="au__solutions__cards w-full max-w-[930px] flex flex-wrap justify-center ">
+            <?php render_who_we_help_cards() ?>
+        </div>
+    </div>
+</section>
--- components/who-we-help/help-hero.php
+++ components/who-we-help/help-hero.php
@@ -17,7 +17,7 @@ function render_slider_points()
         echo '
         <div>
             <div class="flex flex-col items-center">
-                <div class=" w-[400px] mb-4">
+                <div class="w-[80%] mb-4">
                 ' . $animation . '
                 </div>
                 <p class="text-white font-bold mb-2 text-center text-2xl">' . $slide['title'] . '</p>
@@ -31,14 +31,14 @@ function render_slider_points()
 
 <section class="who-we-help-hero w-full bg-dark-blue-background min-w-full relative h-fit lg:h-[90vh] bg-cover flex"
     style="background-image: url(<?php the_field('hero_background_image') ?>); background-position-y: -40px;">
-    <div class="w-7/12 flex flex-col justify-center items-center pl-40">
+    <div class="w-6/12 flex flex-col justify-center items-center pl-40">
         <div class="w-fit">
             <h2 class="text-primary uppercase text-2xl"><?php the_field('title') ?></h2>
             <h1 class="text-white text-6xl"><?php the_field('sub_title') ?></h1>
         </div>
     </div>
-    <div class="w-5/12 flex items-center">
-        <div class="w-1/2">
+    <div class="w-1/2 flex items-center justify-center">
+        <div class="w-[60%]">
             <div class="who-we-help-slider">
                 <?php render_slider_points() ?>
             </div>
