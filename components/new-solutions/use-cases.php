c10f43249c54820185713f0f55d99d08845181ae
Oswaldo Rangel <lebial@gmail.com>
2024-08-21 10:00:51 -0600

    fixed line on solutions use cases

--- components/new-solutions/use-cases.php
+++ components/new-solutions/use-cases.php
@@ -27,8 +27,7 @@ function render_features()
         <div class="table__header lg:border-b border-gray-400 border-solid py-8 lg:py-12">
             <p class="text-2xl text-dark-blue-background mx-auto font-bold text-center">Use Cases</p>
         </div>
-        <div
-            class="table__body use__cases__copy text-dark-blue-background flex justify-center  lg:border-b border-solid border-gray-400">
+        <div class="table__body use__cases__copy text-dark-blue-background flex justify-center ">
             <div class="py-4 px-16 md:px-24  2xl:px-44">
                 <?php the_field('use_cases_copy') ?>
             </div>
