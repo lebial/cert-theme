3c8e14e3a9f25c2a4791e401adc82ca5935f5989
Oswaldo Rangel <lebial@gmail.com>
2024-09-09 18:28:21 -0600

    fixed last feedback

--- components/resources/resources-posts.php
+++ components/resources/resources-posts.php
@@ -35,8 +35,8 @@ function render_articles()
   $img = get_field('post_hero_image', get_the_ID());
   $tags = get_the_tags();
   echo '
-    <div class="ai_insight_card rounded-lg mb-4 p-4 flex flex-col">
-      <div class="ai_card_body shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] p-6 rounded-xl flex-1 flex flex-col">
+    <div class="resources_video_card ai_insight_card rounded-lg mb-4 p-4 flex flex-col">
+      <div class="ai_card_body shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] p-6 rounded-xl flex-1 flex flex-col transition-all duration-300 hover:scale-105">
         <p class="text-gray-400 text-xs mb-4 uppercase">' . $tags[0]->name . '</p>
         <img src="' . $img . '" alt="post thumbnail" class=" rounded-lg my-4"/>
         <h3 class=" text-dark-blue-background text-sm font-bold mb-2">' . get_the_title() . '</h3>
