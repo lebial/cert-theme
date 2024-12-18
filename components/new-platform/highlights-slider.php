2ba66a4a30b5dee2a07524197a4a1f8299c5e0fc
Oswaldo Rangel <lebial@gmail.com>
2024-12-18 12:15:56 -0700

    fixed scaling sizing.

--- components/new-platform/highlights-slider.php
+++ components/new-platform/highlights-slider.php
@@ -6,20 +6,22 @@ function render_hightlight_cards()
 
   foreach ($platform_cards as $card) {
     echo '
-    <div class="platform__highlight__card h-full lg:px-4">
+    <div>
+    <div class="platform__highlight__card h-full lg:px-4 flex flex-col items-center">
       <div class=" platform__highlight__card__body shadow-lg relative flex flex-col items-center w-full bg-dark-blue-background px-3 py-10 rounded-xl h-full overflow-hidden">
         <div class="w-full h-full absolute top-0 left-0 bg-dark-blue-background before:content-[' . "''" . '] before:block before:w-full before:h-full before:bg-dark-blue-background before:bg-opacity-70 before:absolute before:top-0 before:z-20">
           <img src="' . $card['card_background'] . '" alt="card background" class="card__background absolute top-0 left-0 w-full h-full object-cover z-10 "/>
         </div>
         <div class="w-full h-full z-20 flex flex-col items-center">
-          <p class="text-white font-bold text-xl lg:text-2xl text-center mb-2 lg:mb-8">' . $card['card_title'] . '</p>
-          <div class=" w-32 bg-primary h-px mb-2 lg:mb-8"></div>
-          <p class="text-white font-bold text-lg lg:text-xl mb-2 text-center lg:mb-8">' . $card['card_subtitle'] . '</p>
-          <p class="text-white text-sm lg:text-base text-center px-6 mb-2 lg:mb-8">
+          <p class="text-white font-bold text-xl lg:text-2xl text-center mb-2 lg:mb-8 title">' . $card['card_title'] . '</p>
+          <div class=" w-32 bg-primary h-px mb-2 lg:mb-8 card__divider"></div>
+          <p class="text-white font-bold text-lg lg:text-xl mb-2 text-center lg:mb-8 subtitle">' . $card['card_subtitle'] . '</p>
+          <p class="text-white text-sm lg:text-base text-center px-6 mb-2 lg:mb-8 content">
           ' . $card['card_content'] . '
           </p>
         </div>
       </div>
+    </div>
     </div>
     ';
   }
