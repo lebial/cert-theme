73114820c4ad6371b3d9c7a62bc63b95e1c5f5d2
Oswaldo Rangel <lebial@gmail.com>
2024-12-24 11:53:58 -0700

    fixed platform slider (#398)

--- components/new-platform/highlights-slider.php
+++ components/new-platform/highlights-slider.php
@@ -6,8 +6,7 @@ function render_hightlight_cards()
 
   foreach ($platform_cards as $card) {
     echo '
-    <div>
-    <div class="platform__highlight__card h-full lg:px-4 flex flex-col items-center">
+    <div class="platform__highlight__card h-full lg:px-4 items-center">
       <div class=" platform__highlight__card__body shadow-lg relative flex flex-col items-center w-full bg-dark-blue-background px-3 py-10 rounded-xl h-full overflow-hidden">
         <div class="w-full h-full absolute top-0 left-0 bg-dark-blue-background before:content-[' . "''" . '] before:block before:w-full before:h-full before:bg-dark-blue-background before:bg-opacity-70 before:absolute before:top-0 before:z-20">
           <img src="' . $card['card_background'] . '" alt="card background" class="card__background absolute top-0 left-0 w-full h-full object-cover z-10 "/>
@@ -21,7 +20,6 @@ function render_hightlight_cards()
           </p>
         </div>
       </div>
-    </div>
     </div>
     ';
   }
