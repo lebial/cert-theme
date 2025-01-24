3703dce98777bd7a5708827cdc6f2dcf41cf07e1
Oswaldo Rangel <oswaldo.rangel@certilytics.com>
2025-01-24 08:04:10 -0700

    fixed mobile arrows on  homepage and added shadows to platform page slider

--- components/new-platform/highlights-slider.php
+++ components/new-platform/highlights-slider.php
@@ -7,7 +7,10 @@ function render_hightlight_cards()
   foreach ($platform_cards as $card) {
     echo '
     <div class="platform__highlight__card h-full lg:px-4 items-center">
-      <div class=" platform__highlight__card__body shadow-xl relative flex flex-col items-center w-full bg-dark-blue-background px-3 py-10 rounded-xl h-full overflow-hidden">
+      <div class=" platform__highlight__card__body relative flex flex-col items-center w-full bg-dark-blue-background px-3 py-10 rounded-xl h-full overflow-hidden"
+      style="box-shadow: 0 0 20px -4px rgba(0,0,0,.75);
+            -webkit-box-shadow: 0 0 20px -4px rgba(0,0,0,.75);
+            -moz-box-shadow: 0 0 20px -4px rgba(0,0,0,.75);">
         <div class="w-full h-full absolute top-0 left-0 bg-dark-blue-background before:content-[' . "''" . '] before:block before:w-full before:h-full before:bg-dark-blue-background before:bg-opacity-70 before:absolute before:top-0 before:z-20">
           <img src="' . $card['card_background'] . '" alt="card background" class="card__background absolute top-0 left-0 w-full h-full object-cover z-10 "/>
         </div>
